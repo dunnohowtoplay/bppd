9e14f0487b688ab389be7b421c8152335a57694e
dunnohowtoplay <fauzanpartager@gmail.com>
2021-02-06 21:20:39 +0700

    setting up tambah data

--- apps/models.py
+++ apps/models.py
@@ -4,17 +4,17 @@ import datetime
 
 # Create your models here.
 class Kecamatan(models.Model):
-    kecamatan = models.CharField(max_length=30)
+    nama = models.CharField(max_length=30)
 
     def __str__(self):
-        return self.kecamatan
+        return self.nama
 
 class Desa(models.Model):
-    desa = models.CharField(max_length=30)
+    nama = models.CharField(max_length=30)
     kecamatan = models.ForeignKey(Kecamatan, null=True, blank=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.desa
+        return self.nama
 
 class Pendaftaran(models.Model):
     KET_CHOICES = [
--- apps/views.py
+++ apps/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 app_name = 'apps'
 def index(request):
-    return render(request, 'apps/index.html')
+    desas = Desa.objects.all()
+    kecamatans = Kecamatan.objects.all()
+    datas = Pendaftaran.objects.all()
+    context = {
+        'datas':datas,
+        'desas':desas,
+        'kecamatans':kecamatans,
+        }
+    return render(request, 'apps/index.html', context)
