4376cd57f22e3fdcf4d4c15b4916e1cab0ccf817
dunnohowtoplay <fauzanpartager@gmail.com>
2021-02-11 23:31:12 +0700

    pendaftaran delete function done

--- apps/urls.py
+++ apps/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import url
 from . import views
 from dal import autocomplete
 
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.PendaftaranView.as_view(), name='pendaftaran'),
+
+
     url(
         'desa-autocomplete/$',
         views.DesaAutocomplete.as_view(),
--- apps/views.py
+++ apps/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import *
 from .forms import PendaftaranForm
 from django.http import JsonResponse
 from dal import autocomplete
+from django.views.generic import View
 
-
+app_name = 'apps'
 class DesaAutocomplete(autocomplete.Select2QuerySetView):
     def get_queryset(self):
         # Don't forget to filter out results depending on the visitor !
@@ -31,33 +32,53 @@ class KecamatanAutocomplete(autocomplete.Select2QuerySetView):
 
         return qs
 
-app_name = 'apps'
-def index(request):
-    desas = Desa.objects.all()
-    kecamatans = Kecamatan.objects.all()
-    datas = Pendaftaran.objects.all()
-    form = PendaftaranForm()
+class PendaftaranView(View):
+    def get(self, request, *args, **kwargs):
+        datas= Pendaftaran.objects.all()
+        desas = Desa.objects.all()
+        kecamatans = Kecamatan.objects.all()
+        form = PendaftaranForm()
+        context = {
+            'desas':desas,
+            'kecamatans':kecamatans,
+            'datas':datas,
+            'form':form,
+            }
 
-    if request.method == 'POST':
-        form = PendaftaranForm(request.POST)
-        if form.is_valid():
-            form.save()
+        return render(request, 'apps/index.html', context)
 
-        return redirect(index)
-    '''
-    if 'term' in request.GET:
-        qsdesa = Desa.objects.filter(nama__icontains=request.GET.get('term'))
-        listdesa = list()
-        for x in qsdesa:
-            listdesa.append(x.nama)
-        return JsonResponse(listdesa, safe=False)
-    '''
-    context = {
-        'datas':datas,
-        'desas':desas,
-        'kecamatans':kecamatans,
-        'form':form,
-        }
+    def post(self, request, *args, **kwargs):
+        datas=Pendaftaran.objects.all()
+        desas = Desa.objects.all()
+        kecamatans = Kecamatan.objects.all()
+        context = {
+            'datas':datas,
+            'desas':desas,
+            'kecamatans':kecamatans,
+            'datas':datas,
+            }
+
+        if request.method == "POST":
+            tambahform = PendaftaranForm(request.POST)
+            #editdataid = request.POST.get('editid')
+            #editdata = get_object_or_404(Pendaftaran, id = editdataid)
+            #print(editdata)
+            #editform = PendaftaranForm(request.POST or None, instance=editdata)
+            data_ids=request.POST.getlist('deleteid[]')
+            if tambahform.is_valid():
+                tambahform.save()
+
+            #if editform.is_valid():
+                #editform.save()
+
+            for id in data_ids:
+                x = Pendaftaran.objects.get(pk=id)
+                print(x)
+                x.delete()
+            
+            return redirect('pendaftaran')
+        
+        return render(request, 'apps/index.html', context)
     
+        
 
-    return render(request, 'apps/index.html', context)
