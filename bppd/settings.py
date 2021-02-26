d4acd669bf0d7b2b86411231d52efa148c57cb8f
dunnohowtoplay <fauzanpartager@gmail.com>
2021-02-26 20:25:13 +0700

    heroku deploy fix 2

--- bppd/settings.py
+++ bppd/settings.py
@@ -99,7 +99,7 @@ WSGI_APPLICATION = 'bppd.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'bppd_db',
         'USER': 'postgres',
         'PASSWORD': os.environ['DB_PASSWORD'],
